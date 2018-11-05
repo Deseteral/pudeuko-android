4b8e41c26bf507e245974ef77d706062156f55b1
Deseteral <deseteral@gmail.com>
2018-11-06 00:39:57 +0100

    Basic sharing action

--- app/src/main/java/xyz/deseteral/pudeuko/MainActivity.kt
+++ app/src/main/java/xyz/deseteral/pudeuko/MainActivity.kt
@@ -1,12 +1,25 @@
 package xyz.deseteral.pudeuko
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if (intent.type == "text/plain") {
+                    intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+                        Log.i("PUDEUKO", "Intent text: $it")
+                    }
+                }
+            }
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
