93d367526d786dc9ff5e8bc8d0906410293b28a2
Deseteral <deseteral@gmail.com>
2018-11-12 16:30:15 +0100

    Default values for pudeuko object

--- app/src/main/java/xyz/deseteral/pudeuko/domain/PudeukoObject.kt
+++ app/src/main/java/xyz/deseteral/pudeuko/domain/PudeukoObject.kt
@@ -2,7 +2,7 @@ package xyz.deseteral.pudeuko.domain
 
 data class PudeukoObject(
     val text: String,
-    val link: Link?,
-    val image: Image?,
+    val link: Link? = null,
+    val image: Image? = null,
     val createdAt: String
 )
